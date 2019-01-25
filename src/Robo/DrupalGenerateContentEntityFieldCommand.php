7bf2f883b01988875edc79456e9fc85c38b8bab5
jsanford <jsanford@unb.ca>
2019-01-25 09:01:45 -0400

    Improve language, usability of prompting

--- src/Robo/DrupalGenerateContentEntityFieldCommand.php
+++ src/Robo/DrupalGenerateContentEntityFieldCommand.php
@@ -2,9 +2,8 @@
 
 namespace UnbLibraries\DockWorker\Robo;
 
+use Symfony\Component\Console\Helper\Table;
 use Symfony\Component\Finder\Finder;
-use Symfony\Component\Translation\Translator;
-use Symfony\Component\Translation\Loader\ArrayLoader;
 use UnbLibraries\DockWorker\Robo\DrupalCustomEntityCommand;
 
 
@@ -12,12 +11,20 @@ use UnbLibraries\DockWorker\Robo\DrupalCustomEntityCommand;
  * Defines commands in the DrupalGenerateContentEntityFieldCommand namespace.
  */
 class DrupalGenerateContentEntityFieldCommand extends DrupalCustomEntityCommand {
+
   /**
    * The templates available.
    *
    * @var array
    */
   protected $drupalEntityTemplates = [];
+
+  /**
+   * The chosen template.
+   *
+   * @var string
+   */
+  protected $drupalEntityChosenTemplate = NULL;
 
   /**
    * The tokens to replace from the templates.
@@ -40,11 +47,9 @@ class DrupalGenerateContentEntityFieldCommand extends DrupalCustomEntityCommand 
    */
   public function generateContentEntityField() {
     if (!empty($this->drupalCustomEntities)) {
-      $this->getTemplates();
-      foreach ($this->drupalCustomEntities as $real_path => $entity_parser) {
-        $template = $this->io()->choice("What type of field?", $this->drupalEntityTemplates);
-        $this->getOutputTemplates($template, $entity_parser);
-      }
+      $this->setTemplates();
+      $this->setChosenTemplate();
+      $this->getTokenizedTemplateOutputs();
     }
     else {
       $this->say('No modules containing custom entities found.');
@@ -52,45 +57,106 @@ class DrupalGenerateContentEntityFieldCommand extends DrupalCustomEntityCommand 
   }
 
   /**
+   * Set the chosen template.
+   */
+  private function setChosenTemplate() {
+    $this->listTemplates();
+    $value_chosen = FALSE;
+    while ($value_chosen == FALSE) {
+      $template = $this->ask('Enter the template name to use');
+      if (!empty($template)) {
+        if (!empty(($this->drupalEntityTemplates[$template]))) {
+          $this->drupalEntityChosenTemplate = $template;
+          $value_chosen == TRUE;
+          break;
+        }
+        $this->say('Invalid template name.');
+      }
+      else {
+        $this->say('No template entered.');
+      }
+    }
+  }
+
+  /**
+   * Output a formatted list of templates available.
+   */
+  protected function listTemplates() {
+    $wrapped_rows = array_map(
+      function ($template_id, $description) {
+        return [
+          $template_id,
+          $description,
+        ];
+      },
+      array_keys($this->drupalEntityTemplates), $this->drupalEntityTemplates
+    );
+    $table = new Table($this->output());
+    $table->setHeaders(['Template Name', 'Description'])
+      ->setRows($wrapped_rows);
+    $table->setStyle('borderless');
+    $table->render();
+  }
+
+  /**
    * Set the templates available.
    *
    * @hook post-init
    */
-  private function getTemplates() {
+  private function setTemplates() {
     $templates = [];
     $all_templates = new Finder();
     $all_templates->files()->in($this->repoRoot . self::ENTITY_TEMPLATE_PATH)->directories();
 
     foreach ($all_templates as $template) {
-      $this->drupalEntityTemplates[] = $template->getBasename();
+      $this->drupalEntityTemplates[$template->getBasename()] = $this->getTemplateDescription($template->getBasename());
     }
+  }
+
+  /**
+   * Get a template's description.
+   *
+   * @param string $template
+   *   The template to use
+   */
+  private function getTemplateDescription($template) {
+    $template_description_path = $this->getTemplatePath($template) . '/description.txt';
+    var_dump($template_description_path);
+    $template_description = NULL;
+    if (file_exists($template_description_path)) {
+      $template_description = trim(file_get_contents($template_description_path));
+    }
+    if (empty($template_description)) {
+      $template_description = 'No description found.';
+    }
+    return $template_description;
+  }
+
+  private function getTemplatePath($template) {
+    return $this->repoRoot . '/' . self::ENTITY_TEMPLATE_PATH . "/$template";
   }
 
   /**
    * Get the output from all files in this template.
    *
-   * @param string $template
-   *   The template to use
    * @param \PhpParser\Parser $parser
    *   The entity file currently being parsed.
    */
-  private function getOutputTemplates($template, $parser) {
-    $this->setEntityTemplateTokens($template);
-    $template_path = $this->repoRoot . '/' . self::ENTITY_TEMPLATE_PATH . "/$template";
+  private function getTokenizedTemplateOutputs() {
+    $this->setEntityTemplateTokens();
     foreach (self::ENTITY_TEMPLATE_FILES as $template_file) {
-      $this->getOutputTemplateFile($template_path, $template_file);
+      $this->getOutputTemplateFile($template_file);
     }
   }
 
   /**
    * Output the tokenized version of a template file.
    *
-   * @param string $template_path
-   *   The path the template file exists within
    * @param string $template_file
    *   The template file to output.
    */
-  private function getOutputTemplateFile($template_path, $template_file) {
+  private function getOutputTemplateFile($template_file) {
+    $template_path = $this->getTemplatePath($this->drupalEntityChosenTemplate);
     $file_name = $template_path . "/$template_file";
     $this->say($file_name);
     if (file_exists($file_name)) {
@@ -98,7 +164,6 @@ class DrupalGenerateContentEntityFieldCommand extends DrupalCustomEntityCommand 
       foreach ($this->drupalEntityTemplateTokens as $token => $output_value) {
         $contents = str_replace($token, $output_value, $contents);
       }
-
       $this->io->text($contents);
     }
   }
@@ -109,9 +174,10 @@ class DrupalGenerateContentEntityFieldCommand extends DrupalCustomEntityCommand 
    * @param string $template
    *   The template to use
    */
-  private function setEntityTemplateTokens($template) {
+  private function setEntityTemplateTokens() {
+    $chosen_template = $this->drupalEntityChosenTemplate;
     $this->setStandardEntityTemplateTokens();
-    if ($template == 'string') {
+    if ($chosen_template == 'string') {
       $this->setStringEntityTemplateTokens();
     }
   }
@@ -121,29 +187,36 @@ class DrupalGenerateContentEntityFieldCommand extends DrupalCustomEntityCommand 
    */
   private function setStandardEntityTemplateTokens() {
     $this->drupalEntityTemplateTokens['DOCKWORKER_FIELD_MACHINE_NAME'] =
-      $this->askDefault('Field Machine Name / Key', 'user_name');
+      $this->askDefault('Enter the field\'s machine_name/key: ', 'user_name');
 
     $this->drupalEntityTemplateTokens['DOCKWORKER_FIELD_REQUIRED'] =
-      $this->askDefault('Required Field', 'FALSE');
+      $this->confirm('Is this field required?') ? 'TRUE' : 'FALSE';
 
-    $cardinality = $this->askDefault('Cardinality (0 for unlimited)', '1');
+    $cardinality = $this->askDefault('Enter the field\'s cardinality (0 for unlimited):', '1');
     $cardinality = $cardinality == 0 ? 'BaseFieldDefinition::CARDINALITY_UNLIMITED' : $cardinality;
     $this->drupalEntityTemplateTokens['DOCKWORKER_FIELD_CARDINALITY'] = $cardinality;
 
     $this->drupalEntityTemplateTokens['DOCKWORKER_FIELD_LABEL'] =
-      $this->askDefault('Field Label', 'User Name');
+      $this->askDefault('Enter the field\'s form label:', 'User Name');
 
+    $field_class_guess = preg_replace(
+      "/[^A-Za-z0-9]/",
+      '',
+      ucwords(
+        strtolower($this->drupalEntityTemplateTokens['DOCKWORKER_FIELD_LABEL'])
+      )
+    );
     $this->drupalEntityTemplateTokens['DOCKWORKER_FIELD_CAPSCASE'] =
-      $this->askDefault('Field ClassName', 'UserName');
+      $this->askDefault('Enter the pseudo-ClassName to use for the field:', $field_class_guess);
 
     $this->drupalEntityTemplateTokens['DOCKWORKER_FIELD_DESCRIPTION'] =
-      $this->askDefault('Field Description', 'Enter the user name');
+      $this->askDefault('Enter the description (form call to action) for the field:', 'Enter the user name');
 
     $this->drupalEntityTemplateTokens['DOCKWORKER_FIELD_ENTITY_TYPE'] =
-      $this->askDefault('Name of the Parent Entity', 'Bibliographic Record');
+      $this->askDefault('Enter the parent entity label:', 'Bibliographic Record');
 
     $this->drupalEntityTemplateTokens['DOCKWORKER_FIELD_NAMEDSPACED_CLASS'] =
-      $this->askDefault('NameSpaced Parent Class Entity', '\Drupal\yabrm\Entity\BibliographicRecord');
+      $this->askDefault('Enter the parent entity\'s fully namespaced class:', '\Drupal\yabrm\Entity\BibliographicRecord');
   }
 
   /**
@@ -151,9 +224,9 @@ class DrupalGenerateContentEntityFieldCommand extends DrupalCustomEntityCommand 
    */
   private function setStringEntityTemplateTokens() {
     $this->drupalEntityTemplateTokens['DOCKWORKER_FIELD_MAX_LENGTH'] =
-      $this->askDefault('Field Maximum Length', 512);
+      $this->askDefault('Enter the field\'s maximum length:', 512);
     $this->drupalEntityTemplateTokens['DOCKWORKER_FIELD_DEFAULT_VALUE'] =
-      $this->askDefault('Default Value (Empty for None)', '');
+      $this->askDefault('Enter the field\'s default value (empty for no default):', '');
   }
 
 }
