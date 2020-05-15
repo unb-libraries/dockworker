b34d5c89a423096a4ad17c0a9f07f7b8e139f117
Jacob Sanford <jsanford@unb.ca>
2020-05-15 12:07:09 -0300

    DOCKW-23 Boilerplate feature branch deployment

--- src/Robo/Plugin/Commands/DockworkerDockerImageBuildPushCommands.php
+++ src/Robo/Plugin/Commands/DockworkerDockerImageBuildPushCommands.php
@@ -70,9 +70,78 @@ class DockworkerDockerImageBuildPushCommands extends DockworkerDockerImageBuildC
     else {
       $image_name = "{$this->dockerImageName}:$env";
     }
-    $deployment_file = $this->applyKubeDeploymentUpdate($this->repoRoot, $env, $image_name);
-    $deploy_namespace = $this->getKubernetesDeploymentFileNamespace($deployment_file);
-    $this->setRunOtherCommand("deployment:status $deploy_namespace");
+
+    if ($this->environmentIsDeployable($env)) {
+      $deployment_file = $this->applyKubeDeploymentUpdate($this->repoRoot, $env, $image_name);
+      $deploy_namespace = $this->getKubernetesDeploymentFileNamespace($deployment_file);
+      $this->setRunOtherCommand("deployment:status $deploy_namespace");
+    }
+    else {
+      // Deploy a feature branch to k8s.
+      //
+      // @TODO: Add ['metatadata']['labels']['uri] to Ingress
+      //
+      // new method exportAppToYAML(id, namespace, export_dir)
+      //   - check if export_dir empty, otherwise die
+      //   - foreach 'Service', 'Deployment', 'Ingress':
+      //     - export to YAML
+      //
+      // new method copyAppToFeatureBranch(source_namespace, copy_services)
+      //   - foreach copy_services:
+      //     - create temp_dir
+      //     - exportAppToYAML(id, source_namespace, temp_dir)
+      //     - foreach files in temp_dir:
+      //       - $this->prepareObjectForFeatureBranch(file)
+      //     - kubectl create -f tmp_dir
+      //     - delete tmp_dir
+      //
+      // new method prepareObjectForFeatureBranch(file)
+      //   - read file into array
+      //   - uri = file['metatadata']['labels']['uri]
+      //   - feature_uri = {branch_slug}-{uri}
+      //   - switch $array['kind']
+      //     case 'Service':
+      //       - REPLACE metadata.namespace {feature_namespace}
+      //       - REPLACE metadata.labels.vcsRef {feature_uri}
+      //       - REPLACE metadata.labels.uri {feature_uri}
+      //       - REPLACE spec.selector.uri {feature_uri}
+      //     case 'Deployment':
+      //       - foreach containers in spec.template.spec.containers:
+      //        - REPLACE container.image {feature_branch_img}
+      //         - foreach container.env:
+      //           - if is_set env['valueFrom']
+      //             - $this->copyEnvSecretToFeatureNamespace(env_arr, source_env)
+      //         - REMOVE container.volumeMounts
+      //       - REMOVE spec.template.spec.volumes
+      //       - REPLACE metadata.namespace {feature_namespace}
+      //       - REPLACE metadata.labels.vcsRef {feature_uri}
+      //       - REPLACE metadata.labels.uri {feature_uri}
+      //     case 'Ingress':
+      //       - REPLACE metadata.namespace {feature_namespace}
+      //       - REPLACE spec.tls.hosts  {feature_uri}
+      //       - REPLACE spec.rules.host {feature_uri}
+      //
+      // $this->setupFeatureBranchDeployMetadata()
+      //   - feature_namespace= {branch_slug}-{uri}
+      //   - source_namespace = config.feature_branches.data_source (dockworker.yml)
+      //   - required_services = config.feature_branches.required_services (dockworker.yml)
+      //   - copy_services = Append this app_id from deployments/k8s/{source_env}/deployment.yml to required_services
+      //S
+      // $this->cleanupCreateFeatureBranchDeployNamespace()
+      //   - if !dev, !prod: // avoid wiping out deployments
+      //     - kubectl delete namespace {feature_namespace} // Deletes Everything!
+      //     - kubectl create namespace {feature_namespace}
+      //
+      // $this-> copyAppToFeatureBranch(source_namespace, copy_services)
+      //
+      // $this->copyDataToFeatureBranch(source_namespace, feature_namespace)
+      //   Extensions define this. i.e. dockworker-drupal: database, files
+      //
+      // $this->runRequiredMaintenanceTasks(feature_namespace)
+      //   Extensions define this. i.e. dockworker-drupal:
+      //     - Somehow create solr index dynamically?
+      //     - Then : drush sapi-r
+    }
   }
 
   /**
