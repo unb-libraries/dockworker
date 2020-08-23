08daf4ecbddf7d8c49b2c398bb83bb1476e1218b
Jacob Sanford <jsanford@unb.ca>
2020-08-22 23:10:31 -0300

    DOCKW-25 Update badge verbiage

--- src/Robo/Plugin/Commands/DockworkerReadmeBadgeCommands.php
+++ src/Robo/Plugin/Commands/DockworkerReadmeBadgeCommands.php
@@ -45,7 +45,7 @@ class DockworkerReadmeBadgeCommands extends DockworkerCommands {
 $delimiter
 [![Build Status](https://travis-ci.com/unb-libraries/$instance_name.svg?branch=prod)](https://travis-ci.com/unb-libraries/$instance_name)
 [![GitHub license](https://img.shields.io/github/license/unb-libraries/$instance_name)](https://github.com/unb-libraries/$instance_name/blob/prod/LICENSE)
-![GitHub repo size](https://img.shields.io/github/languages/code-size/unb-libraries/$instance_name)
+![GitHub repo size](https://img.shields.io/github/languages/code-size/unb-libraries/$instance_name?label=lean%20repo%20size)
 [![Docker image size](https://img.shields.io/docker/image-size/unblibraries/$instance_name/prod?label=docker%20image%20size)](https://hub.docker.com/repository/docker/unblibraries/$instance_name)
 $delimiter
 EOT;
