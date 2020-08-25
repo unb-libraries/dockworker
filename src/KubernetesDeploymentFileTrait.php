f1c06e1d1fb5c24a6957512a4c36e80be5cd525a
Jacob Sanford <jsanford@unb.ca>
2020-08-25 08:49:08 -0300

    DOCKW-25 Fixes namespace error for getKubernetesDeploymentFileNameFromBranch

--- src/KubernetesDeploymentFileTrait.php
+++ src/KubernetesDeploymentFileTrait.php
@@ -120,7 +120,7 @@ trait KubernetesDeploymentFileTrait {
    * @throws \Dockworker\DockworkerException
    */
   protected function getTokenizedKubeDeploymentFile($repo_root, $env, $image) {
-    $deployment_file = getKubernetesDeploymentFileNameFromBranch($repo_root, $env);
+    $deployment_file = $this->getKubernetesDeploymentFileNameFromBranch($repo_root, $env);
     if (!file_exists($deployment_file)) {
       throw new DockworkerException("Cannot find deployment file [$deployment_file]");
     }
