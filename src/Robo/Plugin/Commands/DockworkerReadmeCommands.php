5c29977956a0af5c62f55486b77256b6dd812281
Jacob Sanford <jsanford@unb.ca>
2021-06-17 15:17:35 -0300

    Rollback inheritance in README

--- src/Robo/Plugin/Commands/DockworkerReadmeCommands.php
+++ src/Robo/Plugin/Commands/DockworkerReadmeCommands.php
@@ -26,31 +26,20 @@ class DockworkerReadmeCommands extends DockworkerCommands {
    * @readmecommand
    */
   public function setApplicationReadme() {
-    $github_contributors = $this->getGitHubContributors($this->gitHubOwner, $this->gitHubRepo);
-    $screenshot_uri = $this->getReadmeScreenshotUri();
-
-    $loader = new \Twig\Loader\FilesystemLoader();
     $repo_readme_overrides = $this->repoRoot . '/.dockworker/documentation/README';
     if (file_exists($repo_readme_overrides)) {
-      $loader->addPath($repo_readme_overrides);
+      array_unshift($this->readMeTemplatePaths, $this->repoRoot . '/.dockworker/documentation/README');
     }
-
-    $dockworker_readme_path = $this->repoRoot . '/vendor/unb-libraries/dockworker/data/README';
-    $this->readMeTemplatePaths[] = $dockworker_readme_path;
-
-    foreach ($this->readMeTemplatePaths as $template_path) {
-      $loader->addPath($template_path, 'base');
-      $loader->addPath($template_path);
-    }
-
+    $this->readMeTemplatePaths[] = $this->repoRoot . '/vendor/unb-libraries/dockworker/data/README';
+    $loader = new \Twig\Loader\FilesystemLoader($this->readMeTemplatePaths);
     $this->readMeTwig = new \Twig\Environment($loader);
     $template = $this->readMeTwig->load('README.md.twig');
     $this->writeReadme(
       $template->render(
         [
           'instance_name' => $this->instanceName,
-          'contributors' => $github_contributors,
-          'screenshot_uri' => $screenshot_uri
+          'contributors' => $this->getGitHubContributors($this->gitHubOwner, $this->gitHubRepo),
+          'screenshot_uri' => $this->getReadmeScreenshotUri()
         ]
       )
     );
