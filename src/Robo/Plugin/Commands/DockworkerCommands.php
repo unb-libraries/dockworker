d76b846ea1066349783333d0dd7f53efff5de3b6
Jacob Sanford <jsanford@unb.ca>
2022-06-08 13:06:04 -0300

    Add helper for warn/confirm destructive

--- src/Robo/Plugin/Commands/DockworkerCommands.php
+++ src/Robo/Plugin/Commands/DockworkerCommands.php
@@ -437,4 +437,38 @@ class DockworkerCommands extends Tasks implements ContainerAwareInterface, Logge
     $io->warning('Destructive, Irreversible Actions Ahead!');
   }
 
+  /**
+   * Determines if the user wishes to proceed with a destructive action.
+   *
+   * @param \Robo\Symfony\ConsoleIO $io
+   *   The IO to use when printing the statement.
+   * @param string $prompt
+   *   The prompt to display to the user.
+   *
+   * @return bool
+   *   TRUE if the user wishes to continue. False otherwise.
+   */
+  protected function warnConfirmDestructiveAction(ConsoleIO $io, string $prompt) : bool {
+    $this->warnDestructiveAction($io);
+    return ($io->confirm($prompt, FALSE));
+  }
+
+  /**
+   * Warns, prompts the user for and conditionally exits the script.
+   *
+   * @param \Robo\Symfony\ConsoleIO $io
+   *   The IO to use when printing the statement.
+   * @param string $prompt
+   *   The prompt to display to the user.
+   */
+  protected function warnConfirmExitDestructiveAction(ConsoleIO $io, string $prompt) {
+    if (
+      $this->warnConfirmDestructiveAction(
+        $io,
+        $prompt
+      ) !== TRUE
+    ) {
+      exit(0);
+    }
+  }
 }
