ae62c48ecf99924ea312d8e03b7407b9030a59b0
Jacob Sanford <jsanford@unb.ca>
2022-09-21 12:01:07 -0300

    Override upstream DockworkerBaseCIServicesWorkflowCommands

--- src/Robo/Plugin/Commands/DockworkerCIServicesWorkflowCommands.php
+++ src/Robo/Plugin/Commands/DockworkerCIServicesWorkflowCommands.php
@@ -3,45 +3,28 @@
 namespace Dockworker\Robo\Plugin\Commands;
 
 use Dockworker\KubernetesDeploymentTrait;
-use Dockworker\RepoCIServicesWorkflowWriterTrait;
-use Dockworker\Robo\Plugin\Commands\DockworkerCommands;
+use Dockworker\Robo\Plugin\Commands\DockworkerBaseCIServicesWorkflowCommands;
 
 /**
- * Defines a class to write a standardized build file to a repository.
+ * Defines a class to write a generic CI workflow file to a repository.
  */
-class DockworkerCIServicesWorkflowCommands extends DockworkerCommands {
-
-  use RepoCIServicesWorkflowWriterTrait;
-  use KubernetesDeploymentTrait;
+class DockworkerCIServicesWorkflowCommands extends DockworkerBaseCIServicesWorkflowCommands {
 
   /**
-   * Writes a standardized CI Services workflow file for this application to this repository.
+   * Defines which CI Services workflow files exist for this application.
    *
-   * @command ci:workflow:file:write
-   * @aliases update-ci-workflow
-   * @aliases uciw
-   *
-   * @actionsworkflowcommand
+   * @return string[]
+   *   An associative array of workflow files supporting this application.
    */
-  public function setApplicationCIServicesWorkflowFile() {
-    $this->CIServicesWorkflowSourcePath = $this->repoRoot . '/vendor/unb-libraries/dockworker/data/gh-actions/deployment-workflow.yaml';
-    $this->writeApplicationCIServicesWorkflowFile();
-  }
-
-  /**
-   * Write out the workflow file.
-   */
-  protected function writeApplicationCIServicesWorkflowFile() {
-    $this->setInstanceName();
-    $tokenized_workflow_contents = file_get_contents($this->CIServicesWorkflowSourcePath);
-    $workflow_contents = str_replace('INSTANCE_NAME', $this->instanceName, $tokenized_workflow_contents);
-    $deployable_env_string = '';
-    foreach ($this->getDeployableEnvironments() as $deploy_env) {
-      $deployable_env_string .= "      refs/heads/$deploy_env\n";
-    }
-    $workflow_contents = str_replace('DEPLOY_BRANCHES', rtrim($deployable_env_string), $workflow_contents);
-    file_put_contents($this->CIServicesWorkflowFilepath, $workflow_contents);
-    $this->say('The updated GitHub actions workflow file has been written.');
+  protected function getCiServicesWorkflowFileDefinitions() : array {
+    return [
+      [
+        'name' => 'Generic build and push workflow',
+        'file_name' => 'deployment-workflow.yaml',
+        'source_path' => 'vendor/unb-libraries/dockworker/data/gh-actions',
+        'repo_path' => '.github/workflows',
+      ]
+    ];
   }
 
 }
