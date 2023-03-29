2f93807721a07c943ef32c02cb51cff322af96c4
Patrick Schilf <pschilf@unb.ca>
2023-03-29 14:40:06 -0300

    Add test commands

---
+++ src/Robo/Plugin/Commands/TestCommands.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Dockworker\Robo\Plugin\Commands;
+
+use Dockworker\DockworkerCommands;
+
+/**
+ * Provides commands for running the application's tests.
+ */
+class TestCommands extends DockworkerCommands
+{
+
+  /**
+   * Execute this application's tests.
+   *
+   * This command is a placeholder for framework-specific test commands that
+   * can be implemented by extensions of this package.
+   *
+   * @command test:all
+   * @aliases test-all
+   */
+  public function runTests(): void
+  {
+    // Pass.
+  }
+
+}
