81dc995b406ef4d69d374df1d5098fef8c0097ea
Jacob Sanford <jsanford@unb.ca>
2023-05-04 13:36:06 -0300

    Add GitHubRepoSettingsTrait.php

---
+++ src/GitHub/GitHubRepoSettingsTrait.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Dockworker\GitHub;
+
+use Dockworker\GitHub\GitHubClientTrait;
+
+/**
+ * Provides methods to authenticate and interact with a GitHub repo's settings.
+ */
+trait GitHubRepoSettingsTrait
+{
+    use GitHubClientTrait;
+
+
+}
