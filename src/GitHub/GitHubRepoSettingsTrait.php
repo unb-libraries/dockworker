ef042bda63543748c2dda0040c9efedcd37c874b
Jacob Sanford <jsanford@unb.ca>
2023-05-04 18:21:22 -0300

    Update GitHubRepoSettingsTrait

--- src/GitHub/GitHubRepoSettingsTrait.php
+++ src/GitHub/GitHubRepoSettingsTrait.php
@@ -10,6 +10,47 @@ use Dockworker\GitHub\GitHubClientTrait;
 trait GitHubRepoSettingsTrait
 {
     use GitHubClientTrait;
+    protected array $gitHubRepositoryTopics = [];
 
+    protected function addGitHubRepositoryTopics(array $topics): void
+    {
+        foreach ($topics as $topic) {
+            $this->addGitHubRepositoryTopic($topic);
+        }
+    }
+
+    protected function addGitHubRepositoryTopic(string $topic): void
+    {
+        $this->gitHubRepositoryTopics[] = strtolower(
+            str_replace(
+                ' ',
+                '-',
+                $topic
+            )
+        );
+    }
+
+    protected function clearGitHubRepositoryTopics(): void
+    {
+        $this->gitHubRepositoryTopics = [];
+    }
+
+    protected function writeGitHubRepositoryTopics($owner, $name): void
+    {
+        $this->gitHubClient->api('repo')->replaceTopics(
+            $owner,
+            $name,
+            $this->gitHubRepositoryTopics
+        );
+    }
+
+    protected function writeGitHubRepositoryDescription($owner, $name, array $description): void
+    {
+        $this->gitHubClient->api('repo')->update(
+            $owner,
+            $name,
+            $description
+        );
+    }
 
 }
