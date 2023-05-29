c0a45740b4b3856b0cbeb86f8c7a1fdcc357bec0
Jacob Sanford <jsanford@unb.ca>
2023-05-29 14:10:22 -0300

    Init repo root before addressing it

--- src/Robo/Plugin/Commands/GitHookInstallCommands.php
+++ src/Robo/Plugin/Commands/GitHookInstallCommands.php
@@ -4,47 +4,46 @@ namespace Dockworker\Robo\Plugin\Commands;
 
 use Dockworker\Cli\CliCommandTrait;
 use Dockworker\DockworkerCommands;
-use Dockworker\System\FileSystemOperationsTrait;
+use Dockworker\RepoFinder;
 
 /**
  * Provides commands to copy git hooks into an application repository.
  */
-class GitHookInstallCommands extends DockworkerCommands
-{
-    use CliCommandTrait;
+class GitHookInstallCommands extends DockworkerCommands {
+  use CliCommandTrait;
 
-    /**
-     * Sets up the required git hooks for dockworker.
-     *
-     * @command git:setup-hooks
-     * @hidden
-     */
-    public function setupGitHooks(): void
-    {
-        $this->copyGitHookFiles('dockworker');
-    }
+  /**
+   * Sets up the required git hooks for dockworker.
+   *
+   * @command git:setup-hooks
+   * @hidden
+   */
+  public function setupGitHooks(): void {
+    $this->applicationRoot = RepoFinder::findRepoRoot();
+    $this->copyGitHookFiles('dockworker');
+  }
 
-    /**
-     * Copies the git hook scripts from a repository into the git hooks path.
-     *
-     * @param string $source_repo
-     *   The repository to copy the git hooks from.
-     */
-    protected function copyGitHookFiles(string $source_repo): void
-    {
-        $cmd = [
-            'rsync',
-            '-a',
-            $this->applicationRoot . "/vendor/unb-libraries/$source_repo/data/scripts/git-hooks/",
-            $this->applicationRoot . '/.git/hooks',
-        ];
-        $this->executeCliCommand(
-            $cmd,
-            null,
-            null,
-            '',
-            '',
-            false
-        );
-    }
+  /**
+   * Copies the git hook scripts from a repository into the git hooks path.
+   *
+   * @param string $source_repo
+   *   The repository to copy the git hooks from.
+   */
+  protected function copyGitHookFiles(string $source_repo): void {
+    $cmd = [
+      'rsync',
+      '-a',
+      $this->applicationRoot . "/vendor/unb-libraries/$source_repo/data/scripts/git-hooks/",
+      $this->applicationRoot . '/.git/hooks',
+    ];
+    $this->executeCliCommand(
+          $cmd,
+          NULL,
+          NULL,
+          '',
+          '',
+          FALSE
+      );
+  }
+
 }
