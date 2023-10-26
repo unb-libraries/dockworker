1b1fe6552f7a050f1cddf05f0ce4f67d5f77c37d
Jacob Sanford <jsanford@unb.ca>
2023-10-26 12:58:05 -0300

    Enable snapshots

---
+++ src/Storage/TemporaryStorageTrait.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Dockworker\Storage;
+
+/**
+ * Provides methods to interact with persistent configuration.
+ */
+trait TemporaryStorageTrait
+{
+
+    public static function createTemporaryStorage(string $identifier = ''): string {
+        $tmp_prefix = "dockworker-$identifier";
+        $tempfile=tempnam(sys_get_temp_dir(), $tmp_prefix);
+        // tempnam creates file on disk
+        if (file_exists($tempfile)) { unlink($tempfile); }
+        mkdir($tempfile);
+        if (is_dir($tempfile)) { return $tempfile; }
+        return '';
+    }
+
+}
