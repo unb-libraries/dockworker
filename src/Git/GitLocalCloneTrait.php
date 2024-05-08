6f6cf8e13f43b7aadc1534f34c1bd23a1a9474db
Jacob Sanford <jsanford@unb.ca>
2024-05-08 07:56:56 -0300

    Allow use of CLI to clone repo

--- src/Git/GitLocalCloneTrait.php
+++ src/Git/GitLocalCloneTrait.php
@@ -18,24 +18,42 @@ trait GitLocalCloneTrait
 
     /**
      * Clones a GitHub repository to a local directory, including all references.
+     *
+     * @param string $owner
+     *   The owner of the repository to clone.
+     * @param string $name
+     *   The name of the repository to clone.
+     * @param string $checkout_branch
+     *   The branch to checkout after cloning.
+     * @param mixed[] $metadata
+     *   Metadata to store in the repository.
+     * @param string $target_directory
+     *   The target directory to clone the repository into.
+     * @param bool $use_cli
+     *   Whether to use the git CLI to clone the repository.
      */
     protected function archiveGitHubRepository(
         string $owner,
         string $name,
         string $checkout_branch = 'dev',
         array $metadata = [],
-        string $target_directory = ''
+        string $target_directory = '',
+        bool $use_cli = false
     ): string {
         $bare_repo_path = $this->createTemporaryLocalStorage("github-clone-$owner-$name-bare");
         if (empty($target_directory)) {
             $repo_path = $this->createTemporaryLocalStorage("github-clone-$owner-$name");
-        }
-        else {
+        } else {
             $repo_path = $target_directory;
         }
         $git = new Git();
-        
-        $git->cloneRepository($this->getGitHubRepositoryCloneUrl($owner, $name), $bare_repo_path, ['--mirror']);
+
+        if ($use_cli) {
+            passthru("git clone --mirror " . $this->getGitHubRepositoryCloneUrl($owner, $name) . " $bare_repo_path");
+        } else {
+            $git->cloneRepository($this->getGitHubRepositoryCloneUrl($owner, $name), $bare_repo_path, ['--mirror']);
+        }
+
         rename($bare_repo_path, $repo_path . '/.git');
 
         $this->applicationRepository = $this->getGitRepoFromPath($repo_path);
@@ -50,8 +68,14 @@ trait GitLocalCloneTrait
     /**
      * Retrieves the clone URL for a GitHub repository.
      */
-    protected function getGitHubRepositoryCloneUrl(string $owner, string $name): string {
-        return "https://github.com/$owner/$name.git";
+    protected function getGitHubRepositoryCloneUrl(
+        string $owner,
+        string $name,
+        bool $use_https = false
+    ): string {
+        if ($use_https) {
+            return "https://github.com/$owner/$name.git";
+        }
+        return "git@github.com:$owner/$name.git";
     }
-
 }
