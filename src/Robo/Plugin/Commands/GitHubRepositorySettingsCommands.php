102cc1dc42913107fac4fcc6db7a9761fd7e4925
Jacob Sanford <jsanford@unb.ca>
2024-07-26 12:02:15 -0300

    Ensure description string does not exceed gihub specs

--- src/Robo/Plugin/Commands/GitHubRepositorySettingsCommands.php
+++ src/Robo/Plugin/Commands/GitHubRepositorySettingsCommands.php
@@ -38,6 +38,9 @@ class GitHubRepositorySettingsCommands extends DockworkerCommands implements Cus
         try {
             $description = Robo::Config()->get('dockworker.application.description');
             $description = preg_replace('/\s+/', ' ', $description);
+            if (strlen($description) > 350) {
+                $description = substr($description, 0, 350) . '...';
+            }
             $uri = Robo::Config()->get('dockworker.application.uri');
             $this->dockworkerIO->block($description);
             $this->dockworkerIO->block($uri);
